2708dc017cab7101178e424f1957fa6bb967dae0
Victor From <victor.from91@gmail.com>
2018-02-27 17:41:25 +0100

    test

--- netlink/netlinkUser.c
+++ netlink/netlinkUser.c
@@ -1,97 +1,107 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <asm/types.h>
 #include <sys/socket.h>
 #include <linux/netlink.h>
 #include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <zconf.h>
+#include <stdlib.h>
 
-#define MAX_PAYLOAD		1024
-#define NETLINK_USER	31
 #define SUCCESS			1
 #define ERROR			-1
+#define MAX_PAYLOAD     4096
+#define NETLINK_USER    31
 
-struct sockaddr_nl s_addr, d_addr;
+struct sockaddr_nl src_addr, dest_addr;
+int sock_fd;
 struct nlmsghdr *nl_hdr = NULL;
 struct iovec iov;
-int fd;
-
-/**
-*	msghdr from socket.h takes 7 parameters,
-*	
-*	void			__user *msg_name;		ptr to socekt adress
-*	int				msg_namelen;			size of socke		
-*	struct iovec	__user *msg_iov;		scatter/gather array
-*	__kernel_size_t	msg_iovlen;				elements in msg_iov
-*	void			__user  msg_control;	anccillary data
-*   __kernel_size_t msg_controllen;			ancillary data buffer length
-*	unsigned int    msg_flags;				flags on recieved message
-*/
-struct msghdr msg; 
+struct msghdr msg;
+void set_src_addr(void);
+void set_dest_addr(void);
+void send_message(char*);
+int recieve_message(void);
+int open_connection(void);
 
 
-int main () {
-	/* source socket setup and bind , netlink.h manpage */
-	fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_USER);
-	
-	/* Check if socket recieved a file descriptor */
-	if(fd < 0) {
-		fprintf(stderr, "socket did not recieve fd");
-		return ERROR;		
-	}
-	
-	memset(&s_addr, 0, sizeof(s_addr));
-	s_addr.nl_family = AF_NETLINK;
-	s_addr.nl_pid = getpid(); /* process own pid*/
-	
-	bind(fd, (struct sockaddr *) &s_addr, sizeof(s_addr));
+int main(int argc, char* argv[]) {
 
-	/* setup netlink header, compute payload with hdr and msg later*/
-	
-	nl_hdr = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD)); 
-	memset(nl_hdr, 0, NLMSG_SPACE(MAX_PAYLOAD));
-	nl_hdr->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
-	nl_hdr->nlmsg_pid = 0;
-	nl_hdr->nlmsg_flags = 0;
-	//nl_hdr->nlmsg_seq = ++sequence_number;
+    printf("Hello, World! This is a test of simple netlink implementation\n");
 
-	/*Add the message to payload (data) */
-	strcpy(NLMSG_DATA(nl_hdr), "User socket message");
-	
-	/*Request ack from kernel*/	
-	//nl_hdr->nlmsg_flags |= NLM_F_ACK;
-	
-	//msg = {&s_addr, sizeof(s_addr), &iov, 1, NULL, 0, 0 };
-	
 
-	
-	/* adress socket bind*/
-	memset(&d_addr, 0, sizeof(d_addr));
-	d_addr.nl_family = AF_NETLINK;
-	d_addr.nl_pid = 0; /*Linux kernel*/
-	d_addr.nl_groups = 0; /* unicast, also supports mulitcast*/
-	
-	iov.iov_base = (void*)nl_hdr;
-	iov.iov_len = nl_hdr->nlmsg_len;
-	msg.msg_name = (void *)&d_addr;
-	msg.msg_namelen = sizeof(d_addr);
-	msg.msg_iov = &iov;
-	msg.msg_iovlen = 1;
-	
-	printf("Sending message: %s \n", (char*)NLMSG_DATA(nl_hdr));
-	/*Sending message to kernel */
-	sendmsg(fd,&msg, 0);
-	
-	printf("Waiting for response.. \n");
-	
-	/* Read the response from kernel */
-	recvmsg(fd, &msg, 0);
-	printf("Recieved message payload: %s \n", (char*)NLMSG_DATA(nl_hdr));
-	
-	/* Close socket connection */
-	free(nl_hdr);
-	close(fd);	
-	
-	return SUCCESS;
+    //TODO send in correct params for both src and dest
+    set_src_addr();
+    set_dest_addr();
+    if(open_connection() != 0){
+        fprintf(stderr, "Could not connect\n");
+        return ERROR;
+    }
+
+    send_message("Hello kernel");
+    return SUCCESS;
+}
+
+void set_src_addr(){
+
+    memset(&src_addr, 0, sizeof(src_addr));
+    src_addr.nl_family = AF_NETLINK;
+
+    /*If pid is set before bind, application has the responsibility to add unique pid to each connection
+     * identifies a socket, not a process. */
+    src_addr.nl_pid = getpid();
+
+    //src_addr.nl_groups = 0-32 , default is 0, each family has 23 grops.
+    //src_addr.nl_pad = 0; , used for zeroing
+
+}
+
+void set_dest_addr(){
+
+    memset(&dest_addr, 0, sizeof(src_addr));
+    dest_addr.nl_family = AF_NETLINK;
+    dest_addr.nl_pid = 0;
+    dest_addr.nl_groups = 0;
+
+}
+
+void send_message(char *message){
+
+    //struct iovec iov = {nl_hdr, nl_hdr->nlmsg_len};
+    //struct msghdr msg =  { &dest_addr, sizeof(dest_addr), &iov, 1, NULL, 0, 0 };
+
+    nl_hdr = (struct nlmsghdr*)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+    memset(nl_hdr, 0 , sizeof(nl_hdr));
+    nl_hdr->nlmsg_len = NLMSG_LENGTH(NLMSG_SPACE(sizeof(message)));
+    nl_hdr->nlmsg_pid = 0;
+    nl_hdr->nlmsg_flags = 0;
+
+    iov.iov_base = (void*)nl_hdr;
+    iov.iov_len = nl_hdr->nlmsg_len;
+    msg.msg_name = (void *)&dest_addr;
+    msg.msg_namelen = sizeof(dest_addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+
+    strcpy(NLMSG_DATA(nl_hdr), message);
+
+    sendmsg(sock_fd, &msg, 0);
+    printf("PID-%d sending with payload:\n%s\n", src_addr.nl_pid,(char*)NLMSG_DATA(nl_hdr));
+
+}
+
+int open_connection(){
+    sock_fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_USER);
+
+    if(sock_fd < 0){
+        fprintf(stderr, "problem connection to socket\n");
+        return ERROR;
+    }
+
+    if(bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr)) != 0){
+        fprintf(stderr, "Error binding socket\n");
+        return ERROR;
+    }
+
+    printf("successfully binding socket!\n");
+
+    return SUCCESS;
 }
